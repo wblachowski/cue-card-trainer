9d33e48d559be95a9ae9b61a9d1b53050693b4e0
Wojciech Blachowski <wblachowsky@gmail.com>
2020-12-16 20:02:26 +0100

    Add basic view

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Platform,
+  Button,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
+  const [cue, setCue] = useState("No cues");
+
+  const displayNextCue = () => {
+    console.log("Next cue");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>{cue}</Text>
+      <Button title="Next" onPress={displayNextCue}></Button>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
