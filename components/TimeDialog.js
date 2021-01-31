d5d6ff0e40fe46e8b105cec52821f3ec1e8cd573
Wojciech Blachowski <wblachowsky@gmail.com>
2021-01-31 19:15:19 +0100

    Adjust dark mode in time dialogs

--- components/TimeDialog.js
+++ components/TimeDialog.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Text, View, TextInput, StyleSheet } from "react-native";
 import Dialog from "react-native-dialog";
 import { useEffect } from "react/cjs/react.development";
+import { useDarkMode } from "react-native-dark-mode";
 
 export default function TimeDialog({
   title,
@@ -14,6 +15,7 @@ export default function TimeDialog({
 }) {
   const [minutes, setMinutes] = useState(initMinutes);
   const [seconds, setSeconds] = useState(initSeconds);
+  const isDarkMode = useDarkMode();
 
   useEffect(() => setMinutes(initMinutes), [initMinutes]);
   useEffect(() => setSeconds(initSeconds), [initSeconds]);
@@ -25,9 +27,19 @@ export default function TimeDialog({
   };
 
   return (
-    <Dialog.Container visible={visible} onBackdropPress={onClose}>
-      <Dialog.Title>{title}</Dialog.Title>
-      <Dialog.Description>{description}</Dialog.Description>
+    <Dialog.Container
+      visible={visible}
+      onBackdropPress={onClose}
+      contentStyle={{
+        backgroundColor: isDarkMode ? "black" : "white",
+      }}
+    >
+      <Dialog.Title style={{ color: isDarkMode ? "white" : "black" }}>
+        {title}
+      </Dialog.Title>
+      <Dialog.Description style={{ color: isDarkMode ? "white" : "black" }}>
+        {description}
+      </Dialog.Description>
       <View
         style={{
           flexDirection: "row",
@@ -42,8 +54,20 @@ export default function TimeDialog({
           maxLength={2}
           placeholder={initMinutes}
           onChangeText={(text) => setMinutes(text)}
+          placeholderTextColor={
+            isDarkMode ? "rgba(255,255,255,0.6)" : "rgba(0,0,0,0.6)"
+          }
+          style={{ color: isDarkMode ? "white" : "black" }}
         />
-        <Text style={{ position: "absolute", bottom: 30 }}>:</Text>
+        <Text
+          style={{
+            position: "absolute",
+            bottom: 30,
+            color: isDarkMode ? "white" : "black",
+          }}
+        >
+          :
+        </Text>
         <Dialog.Input
           underlineColorAndroid="blue"
           style={styles.timeInputStyle}
@@ -52,6 +76,10 @@ export default function TimeDialog({
           maxLength={2}
           placeholder={initSeconds}
           onChangeText={(text) => setSeconds(text)}
+          placeholderTextColor={
+            isDarkMode ? "rgba(255,255,255,0.6)" : "rgba(0,0,0,0.6)"
+          }
+          style={{ color: isDarkMode ? "white" : "black" }}
         />
       </View>
       <Dialog.Button label="Cancel" onPress={onClose} />
