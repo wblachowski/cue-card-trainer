cd91b123e180e2595d08ded5406531722afcef0e
Wojciech Blachowski <wblachowsky@gmail.com>
2021-02-15 22:32:40 +0100

    Add light grey color

--- styles/colors.js
+++ styles/colors.js
@@ -1,6 +1,7 @@
 export const PRIMARY = "#2196f3";
 export const LIGHT = "#6ec6ff";
 export const DARK = "#0069c0";
-export const DARK_GREY = "#121212";
 export const GREY = "#767577";
+export const DARK_GREY = "#121212";
+export const LIGHT_GREY = "#d3d3d3";
 export const WHITE = "#ffffff";
