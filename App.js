0595b90f911de039be73eff7b141275856ee3216
Wojciech Blachowski <wblachowsky@gmail.com>
2021-02-17 16:08:08 +0100

    Add paper wrapper

--- App.js
+++ App.js
@@ -1,29 +1,43 @@
 import React from "react";
 import {
   NavigationContainer,
-  DarkTheme,
-  DefaultTheme,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
 } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useDarkMode } from "react-native-dark-mode";
 import Main from "./scenes/Main";
 import Settings from "./scenes/Setttings";
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
+import merge from "deepmerge";
 
 const Stack = createStackNavigator();
+const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
 
 export default function App() {
   const isDarkMode = useDarkMode();
 
   return (
-    <NavigationContainer theme={isDarkMode ? DarkTheme : DefaultTheme}>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Main}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Settings" component={Settings} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider
+      theme={isDarkMode ? CombinedDarkTheme : CombinedDefaultTheme}
+    >
+      <NavigationContainer
+        theme={isDarkMode ? CombinedDarkTheme : CombinedDefaultTheme}
+      >
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Main}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Settings" component={Settings} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
