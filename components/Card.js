eacb2f1eb966d3c72cadb25f5769ccb56b35de04
Wojciech Blachowski <wblachowsky@gmail.com>
2021-03-05 11:31:48 +0100

    Refactor styles

--- components/Card.js
+++ components/Card.js
@@ -1,50 +1,42 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Text } from "react-native-paper";
 
 export default function Card({ card }) {
   return (
     <View>
-      <Text
-        style={{
-          fontSize: 18,
-          fontWeight: "bold",
-          marginBottom: 16,
-        }}
-      >
-        {card.title}
-      </Text>
-      <Text
-        style={{
-          fontSize: 14,
-          marginTop: 2,
-          marginBottom: 2,
-        }}
-      >
-        {card.prompt}
-      </Text>
+      <Text style={styles.titleText}>{card.title}</Text>
+      <Text style={styles.promptText}>{card.prompt}</Text>
       {(card.bullets || []).map((bullet, i) => (
-        <Text
-          key={i}
-          style={{
-            fontSize: 14,
-            marginLeft: 20,
-            marginTop: 2,
-            marginBottom: 2,
-          }}
-        >
+        <Text key={i} style={styles.bulletText}>
           • {bullet}
         </Text>
       ))}
-      <Text
-        style={{
-          fontSize: 14,
-          marginTop: 2,
-          marginBottom: 2,
-        }}
-      >
-        {card.ending}
-      </Text>
+      <Text style={styles.endingText}>{card.ending}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  promptText: {
+    fontSize: 14,
+    marginTop: 2,
+    marginBottom: 2,
+  },
+  bulletText: {
+    fontSize: 14,
+    marginLeft: 20,
+    marginTop: 2,
+    marginBottom: 2,
+  },
+  endingText: {
+    fontSize: 14,
+    marginTop: 2,
+    marginBottom: 2,
+  },
+});
